3e0fb54f3a88aaadd4b051e691b040c896ee8dfb
Noorbilu <nuralabonno@gmail.com>
2026-02-20 00:30:18 +0600

    changed code

--- bankerslaw.c
+++ bankerslaw.c
@@ -1,1 +1,120 @@
+#include <stdio.h>
 
+int main() {
+    int n, r;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    printf("Enter number of resources: ");
+    scanf("%d", &r);
+
+    int alloc[n][r], max[n][r], need[n][r];
+    int total[r], available[r];
+    int finish[n];
+    int safeSeq[n];
+
+    // Input Allocation matrix
+    printf("\nEnter Allocation Matrix:\n");
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < r; j++) {
+            scanf("%d", &alloc[i][j]);
+        }
+    }
+
+    // Input Max matrix
+    printf("\nEnter Max Matrix:\n");
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < r; j++) {
+            scanf("%d", &max[i][j]);
+        }
+    }
+
+    // Input Total Resources
+    printf("\nEnter Total Resources:\n");
+    for(int i = 0; i < r; i++) {
+        scanf("%d", &total[i]);
+    }
+
+    // Calculate Available
+    for(int i = 0; i < r; i++) {
+        int sum = 0;
+        for(int j = 0; j < n; j++) {
+            sum += alloc[j][i];
+        }
+        available[i] = total[i] - sum;
+    }
+
+    // Calculate Need = Max - Allocation
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < r; j++) {
+            need[i][j] = max[i][j] - alloc[i][j];
+        }
+    }
+
+    // Print Need Matrix
+    printf("\nNeed Matrix:\n");
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < r; j++) {
+            printf("%d ", need[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Print Available Resources
+    printf("\nAvailable Resources:\n");
+    for(int i = 0; i < r; i++) {
+        printf("%d ", available[i]);
+    }
+    printf("\n");
+
+    // Initialize finish
+    for(int i = 0; i < n; i++) {
+        finish[i] = 0;
+    }
+
+    int count = 0;
+
+    while(count < n) {
+        int found = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(finish[i] == 0) {
+                int flag = 1;
+
+                for(int j = 0; j < r; j++) {
+                    if(need[i][j] > available[j]) {
+                        flag = 0;
+                        break;
+                    }
+                }
+
+                if(flag) {
+                    for(int j = 0; j < r; j++) {
+                        available[j] += alloc[i][j];
+                    }
+
+                    safeSeq[count++] = i;
+                    finish[i] = 1;
+                    found = 1;
+                }
+            }
+        }
+
+        if(found == 0) {
+            break;
+        }
+    }
+
+    if(count == n) {
+        printf("\nSystem is in SAFE state.\nSafe sequence: ");
+        for(int i = 0; i < n; i++) {
+            printf("P%d ", safeSeq[i]);
+        }
+        printf("\n");
+    } else {
+        printf("\nSystem is NOT in safe state (Deadlock possible).\n");
+    }
+
+    return 0;
+}
--- priorityNonPreemtive.c
+++ priorityNonPreemtive.c
@@ -2,10 +2,9 @@
 
 int main() {
     int n, time = 0, completed = 0;
-    int at[20], bt[20], pr[20];
+    int at[20], bt[20], pr[20], done[20] = {0};
     int ct[20], tat[20], wt[20];
-    int done[20] = {0};
-    float avg_wt = 0, avg_tat = 0;
+    float avg_tat = 0, avg_wt = 0;
 
     printf("Enter number of processes: ");
     scanf("%d", &n);
@@ -24,34 +23,34 @@ int main() {
         int idx = -1;
         int best_pr = 9999;
 
+        // Find process with highest priority that has arrived and not done
         for(int i = 0; i < n; i++) {
             if(at[i] <= time && done[i] == 0) {
                 if(pr[i] < best_pr) {
                     best_pr = pr[i];
                     idx = i;
+                } else if(pr[i] == best_pr) {
+                    if(at[i] < at[idx]) idx = i; // tie-breaker: earlier arrival
                 }
             }
         }
 
         if(idx != -1) {
-            time += bt[idx];
-            ct[idx] = time;
-            done[idx] = 1;
+            time += bt[idx];     // Execute process fully
+            ct[idx] = time;      // Completion time
+            done[idx] = 1;       // Mark done
             completed++;
         } else {
-            time++;  // CPU idle
+            time++; // CPU idle
         }
     }
 
     printf("\nP\tAT\tBT\tPR\tCT\tTAT\tWT\n");
-
     for(int i = 0; i < n; i++) {
         tat[i] = ct[i] - at[i];
         wt[i] = tat[i] - bt[i];
-
         avg_tat += tat[i];
         avg_wt += wt[i];
-
         printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
                i+1, at[i], bt[i], pr[i], ct[i], tat[i], wt[i]);
     }
