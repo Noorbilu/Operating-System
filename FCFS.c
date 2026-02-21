61a4621fcdd35e143028d2d3b48a672fc0f029e2
Noorbilu <nuralabonno@gmail.com>
2026-02-21 23:14:58 +0600

    changed code

--- FCFS.c
+++ FCFS.c
@@ -1,64 +1,43 @@
 #include <stdio.h>
-
-int main() {
-    int n;
-    int at[20], bt[20];
-    int ct[20], tat[20], wt[20];
-    float avg_wt = 0, avg_tat = 0;
+int main()
+{
+    int bt[20], at[20], wt[20], tat[20], ct[20], i, n;
+    float wtavg = 0, tatavg = 0;
 
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    for(int i = 0; i < n; i++) {
-        printf("\nProcess P%d\n", i+1);
+    for (i = 0; i < n; i++)
+    {
+        printf("\nProcess P%d\n", i + 1);
         printf("Arrival Time: ");
         scanf("%d", &at[i]);
         printf("Burst Time: ");
         scanf("%d", &bt[i]);
     }
 
-    // Sort by Arrival Time (FCFS rule)
-    for(int i = 0; i < n-1; i++) {
-        for(int j = i+1; j < n; j++) {
-            if(at[i] > at[j]) {
-                // swap arrival time
-                int temp = at[i];
-                at[i] = at[j];
-                at[j] = temp;
+    int time = 0;
 
-                // swap burst time
-                temp = bt[i];
-                bt[i] = bt[j];
-                bt[j] = temp;
-            }
-        }
+    for (i = 0; i < n; i++)
+    {
+        if (time < at[i])
+            time = at[i]; // CPU idle until process arrives
+
+        wt[i] = time - at[i];
+        time += bt[i];
+        ct[i] = time;
+        tat[i] = bt[i] + wt[i];
+
+        wtavg += wt[i];
+        tatavg += tat[i];
     }
 
-    // Calculate Completion Time
-    ct[0] = at[0] + bt[0];
+    printf("\nProcess\tAT\tBT\tCT\tWT\tTAT\n");
+    for (i = 0; i < n; i++)
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n", i + 1, at[i], bt[i], ct[i], wt[i], tat[i]);
 
-    for(int i = 1; i < n; i++) {
-        if(ct[i-1] < at[i])
-            ct[i] = at[i] + bt[i];  // CPU idle case
-        else
-            ct[i] = ct[i-1] + bt[i];
-    }
-
-    printf("\nP\tAT\tBT\tCT\tTAT\tWT\n");
-
-    for(int i = 0; i < n; i++) {
-        tat[i] = ct[i] - at[i];
-        wt[i] = tat[i] - bt[i];
-
-        avg_tat += tat[i];
-        avg_wt += wt[i];
-
-        printf("P%d\t%d\t%d\t%d\t%d\t%d\n",
-               i+1, at[i], bt[i], ct[i], tat[i], wt[i]);
-    }
-
-    printf("\nAverage Turnaround Time = %.2f", avg_tat/n);
-    printf("\nAverage Waiting Time = %.2f\n", avg_wt/n);
+    printf("\nAverage Waiting Time = %.2f\n", wtavg / n);
+    printf("Average Turnaround Time = %.2f\n", tatavg / n);
 
     return 0;
 }
